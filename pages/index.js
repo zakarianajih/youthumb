7e1b6a98fe6cf4edb38c9418fcea27898005a3ad
zakarianajih <zakarianajih@github.com>
2023-09-29 17:15:06 +0100

    add sesc

--- pages/index.js
+++ pages/index.js
@@ -35,7 +35,7 @@ const Index = () => {
 
   return (
     <div className="container mx-auto px-4 py-8">
-      <header className="text-center mb-8" >
+      <header>
         <h1 className="title-webites">
           Free YouTube Thumbnail Downloader
         </h1>
